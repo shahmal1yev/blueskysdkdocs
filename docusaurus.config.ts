7ee15622f1ba713ff1008dec85dbc8dabaf725bb
Eldar Shahmaliyev <shahmal1yevv@proton.me>
2024-12-09 00:26:16 +0400

    Update statics

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -82,7 +82,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/social-card.webp',
     navbar: {
       title: 'BlueSky SDK',
       logo: {
